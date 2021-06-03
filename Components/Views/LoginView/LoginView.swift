e40a17c94fa7011d560cead1b50fb404c0ae56f0
Alexandru <aual@gft.com>
2021-06-03 12:29:18 +0200

    add collectionView add LoginCell add LoginView add ThemeColors

---
+++ Components/Views/LoginView/LoginView.swift
@@ -0,0 +1,34 @@
+//
+//  LoginView.swift
+//  MessageApp
+//
+//  Created by Apostol, Alexandru on 03/06/2021.
+//
+
+import UIKit
+
+class LoginView: UIView {
+    lazy var textField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.placeholder = "Email: "
+        return textField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        self.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 16).isActive = true
+        textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 16).isActive = true
+        textField.topAnchor.constraint(equalTo: self.topAnchor,constant: 16).isActive = true
+        textField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 16).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- MessageApp/Modules/Login/View/LoginViewController.swift
+++ MessageApp/Modules/Login/View/LoginViewController.swift
@@ -9,10 +9,54 @@ import UIKit
 
 class LoginViewController: UIViewController {
     let presenter: LoginViewControllerPresenter
+    private let sectionInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    
+    lazy var collectionViewContainer: UIView = {
+        let view = UIView(frame: .zero)
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        return view
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.register(LoginCell.self, forCellWithReuseIdentifier: "loginView")
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+       var layout = UICollectionViewFlowLayout()
+        layout.sectionInset = sectionInsets
+        layout.itemSize = CGSize(width: view.frame.size.width, height: 140)
+        return layout
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = UIColor.theme(identifier: .messageAppDefaultPurple)
+        self.setUpLayout()
+    }
+    
+    private func setUpLayout() {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        setUpCollectionViewContainerLayout()
+    }
+    
+    private func setUpCollectionViewContainerLayout() {
+        self.view.addSubview(collectionViewContainer)
+        let guide = view.safeAreaLayoutGuide
+        self.collectionViewContainer.translatesAutoresizingMaskIntoConstraints = false
+        self.collectionViewContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        self.collectionViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        self.collectionViewContainer.topAnchor.constraint(equalTo: guide.topAnchor, constant: 16).isActive = true
+        self.collectionViewContainer.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     init(presenter: LoginViewControllerPresenter) {
@@ -29,3 +73,15 @@ class LoginViewController: UIViewController {
 extension LoginViewController: LoginViewControllerProtocol {
     
 }
+
+extension LoginViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "loginView", for: indexPath) as! LoginCell
+        return cell
+    }
+}
+
